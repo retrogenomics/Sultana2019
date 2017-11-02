7dbb1af6633cf4dfbc687afcb05410716c983d53
Gael Cristofari <Gael.Cristofari@unice.fr>
2017-11-02 17:29:43 +0100

    added a filter to remove insertions with "echo" due to homopolymer tracts

--- L1 insertion calling/171031_atlas-neo_all_samples.sh
+++ L1 insertion calling/171031_atlas-neo_all_samples.sh
@@ -16,11 +16,13 @@ REF_GENOME="hg19"
 DUKE_FILTER="${BIOINFO}/annotations/hg19/other/hg19.wgEncodeDukeMapabilityRegionsExcludable.bed"
 ENCODE_FILTER="${BIOINFO}/annotations/hg19/other/hg19.wgEncodeDacMapabilityConsensusExcludable.bed"
 HELA_GENOME="${BIOINFO}/annotations/hg19/other/hg19.helaMainChromosomes.bed"
-
+s="************"
 # make result directory
 mkdir -p ${OUTPUT_DIR}
 
 # read EXP_CONFIG file
+echo -e "\n$s Read experiment metadata $s"
+
 i=1
 while read aLine ;
 do
@@ -37,13 +39,15 @@ done < "${EXP_CONFIG}"
 
 # identify independent sequencing runs
 uniq_run=($( printf "%s\n" ${run[*]} | sort -u ))
-echo ${uniq_run}
 
 # call atlas-clustering-neo.sh script for each run
 # note that it assumes that a given run is associated to a unique .fastq file
 
+echo -e "\n$s L1 insertion calling $s"
+
 for value in ${uniq_run[*]};
 do
+	echo -ne "Run ${value}..."
 	# generate bc file for each run
 	echo -ne "" > "${OUTPUT_DIR}/BC_R${value}.txt"
 	for i in $( seq 1 ${#run[@]} );
@@ -57,12 +61,20 @@ do
   	# run identify insertion in each run
 #    	atlas-clustering-neo.sh -o "${OUTPUT_DIR}" -b "${OUTPUT_DIR}/BC_R${value}.txt" "${DATA}/${fastq}"
 #    	mv "${OUTPUT_DIR}/global.neo.3atlas.v${VERSION}.log" "${OUTPUT_DIR}/R${value}_global.neo.3atlas.v${VERSION}.log"
+	rm "${OUTPUT_DIR}/BC_R${value}.txt"
+	echo -e "Done"
 done
 
 # multi-atlas-compare within each run
+
+echo -e "\n$s Intra-run comparisons $s"
+
 cd "${OUTPUT_DIR}"
+echo -ne "" > Blacklist_atlas.bed
+
 for value in ${uniq_run[*]};
 do
+	echo -e "\n$s Run #${value}"
 	name_list="";
 	file_list="";
 	for file in R${value}*.neo.3atlas.v${VERSION}.insertions.true.bed ;
@@ -99,10 +111,32 @@ do
 
 	field=$( echo | awk -v n=$n '{for (i=1;i<n;i++) {printf i","}; printf n}' )
 
+# 	sort -k1,1 -k2,2n multi-atlas-compare.NB_NRR.R${value}.tmp \
+# 	| bedtools intersect -v -a - -b ${DUKE_FILTER} \
+# 	| bedtools intersect -v -a - -b ${ENCODE_FILTER} \
+# 	| bedtools intersect -a - -b ${HELA_GENOME} \
+# 	| bedtools merge -s -d 500 -i - -c $field -o collapse \
+# 	| awk -v OFS="\t" -v n=$n -v h=$h '\
+# 		BEGIN {samples=n-7}
+# 		($5!~/,/) {for (i=5;i<n+4;i++) {printf $i "\t"}; printf $(n+4) "\n" }
+# 		($5~/,/) {
+# 			best=1
+# 			highest=0
+# 			for (i=NF-samples+1; i<=NF; i++) {
+# 				k=split($i,cov,",")
+# 				for (j=1;j<=k;j++) {
+# 					if (cov[j]>highest){highest=cov[j]; best=j}
+# 				}
+# 			}
+# 			for (i=1;i<=n;i++) {
+# 				split($(i+4),f,",")
+# 				if (i<n) {printf f[best] "\t"} else {printf f[best] "\n"}
+# 			}
+# 		}
+#  	' \
+# 	>> multi-atlas-compare.NB_NRR.R${value}.tab
+
 	sort -k1,1 -k2,2n multi-atlas-compare.NB_NRR.R${value}.tmp \
-	| bedtools intersect -v -a - -b ${DUKE_FILTER} \
-	| bedtools intersect -v -a - -b ${ENCODE_FILTER} \
-	| bedtools intersect -a - -b ${HELA_GENOME} \
 	| bedtools merge -s -d 500 -i - -c $field -o collapse \
 	| awk -v OFS="\t" -v n=$n -v h=$h '\
 		BEGIN {samples=n-7}
@@ -124,17 +158,23 @@ do
  	' \
 	>> multi-atlas-compare.NB_NRR.R${value}.tab
 
+	# create a black list of excluded insertions for inter-run comparison
+	bedtools intersect -v -wa -a multi-atlas-compare.NB_NRR.R${value}.tmp -b multi-atlas-compare.NB_NRR.R${value}.tab \
+	| sort -k1,1 -k2,2n \
+	| cut -f1-6 \
+	>> Blacklist_atlas.bed
+
 	rm multi-atlas-compare.NB_NRR.R${value}.tmp
 
 	# correct ID name and coordinates for intra-run comparison
 	sed "s/EXP/R${value}/g" "multi-atlas-compare.NB_NRR.R${value}.tab" \
 	| awk '{OFS="\t"; if ($1~/^#/) {print $1,$2,$3,$4,"NB_SAMPLES",$6} else {printf $1 "\t%.0f\t",($3+$2)/2; printf "%.0f\t",($3+$2)/2; printf $4 "\t" $5 "\t" $6 "\n"}}' \
 	> "R${value}.insertions.true.bed" ;
-
-	echo ;
 done
 
 # multi-atlas-compare between runs
+echo -e "\n$s Inter-run comparison and insertion filtering $s"
+
 name_list="";
 file_list="";
 for value in ${uniq_run[*]};
@@ -146,13 +186,20 @@ done;
 name_list=$(echo ${name_list%?});
 file_list=$( echo ${file_list%?} );
 atlas-compare-samples.sh -n ${name_list} -s NB_SAMPLES ${file_list} ;
-atlas-compare-samples.sh -n ${name_list} -s NB_NRR ${file_list} ;
 
 # reformat coordinates to take into account bedtools merge bug on 0-length intervals
 name_list2=$( echo -ne ${name_list} | awk -F"," '{OFS="."; $NF=$NF; print $0}' )
 
+# awk '$1~/^#/ {print $0} $1!~/^#/ {printf $1"\t%.0f\t",($3+$2)/2; printf "%.0f\t",($3+$2)/2; for (i=4; i<NF; i++) {printf $i "\t"}; printf $NF "\n"}' multi-atlas-compare.NB_SAMPLES.${name_list2}.tab \
+# | sort -k1,1 -k2,2n \
+# > "R${uniq_run[0]}-R${uniq_run[-1]}.insertions.tab"
+
 awk '$1~/^#/ {print $0} $1!~/^#/ {printf $1"\t%.0f\t",($3+$2)/2; printf "%.0f\t",($3+$2)/2; for (i=4; i<NF; i++) {printf $i "\t"}; printf $NF "\n"}' multi-atlas-compare.NB_SAMPLES.${name_list2}.tab \
 | sort -k1,1 -k2,2n \
+| bedtools intersect -v -a - -b ${DUKE_FILTER} \
+| bedtools intersect -v -a - -b ${ENCODE_FILTER} \
+| bedtools intersect -v -a - -b Blacklist_atlas.bed \
+| bedtools intersect -a - -b ${HELA_GENOME} \
 > "R${uniq_run[0]}-R${uniq_run[-1]}.insertions.tab"
 
 cut -f1-6 "R${uniq_run[0]}-R${uniq_run[-1]}.insertions.tab" \
