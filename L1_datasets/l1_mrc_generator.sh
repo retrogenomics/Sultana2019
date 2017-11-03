64baeffed4714f5a6b33100bdb51c2a27295ef9c
Gael Cristofari <Gael.Cristofari@unice.fr>
2017-11-03 12:12:07 +0100

    random picking can be restricted to a subset of regions provided in a bed file

--- L1_datasets/l1_mrc_generator.sh
+++ L1_datasets/l1_mrc_generator.sh
@@ -1,27 +1,45 @@
-####### load preferences and define global settings
-script_name="bmc_generator"
-script_version='1.0'
+#!/bin/bash
+
+#################################################################################
+# Script to generate a matched random control dataset where intervals have
+# the same size and same base composition as input dataset
+#################################################################################
+
+#################################################################################
+# Set global parameters, variables and folders
+#################################################################################
+
+script_name="l1_mrc_generator"
+script_version='2.0'
 
 INPUT_FILE=""
 OUTPUT_FILE=""
 GENOME=""
+ALLOWED=""
 
 # store usage explanations
 USAGE="\
 $script_name v$script_version:\tStarting from a .bed file with intervals of fixed length (short),\n\
 generates a random .bed file with intervals of identical size and matching base composition.\n\
-usage:\t$( basename $0 ) [options] -g <genome.fa> -i <input.bed> -o <output.bed>\n\
+usage:\t$( basename $0 ) [options] -g <genome.fa> -a <included regions.bed> -i <input.bed> -o <output.bed>\n\
 options:\n\
+\t-a Instead of randomly picking features in a genome, the -a option defines a BED file of coordinates in which
+\t   new intervals should be randomly picked
 \t-h Print this help menu. \n\
 \t-v What version of $script_name are you using? \n\
 "
 
+#################################################################################
+# Parse script arguments and verify parameters
+#################################################################################
+
 # parse script arguments
-while getopts 'hvg:i:o:' opt ; do
+while getopts 'hvag:i:o:' opt ; do
 	case $opt in
 		g) GENOME=$OPTARG ;;
 		i) INPUT_FILE=$OPTARG ;;
 		o) OUTPUT_FILE=$OPTARG ;;
+		a) ALLOWED=$OPTARG ;;
 		h) echo -e "\n$USAGE"; exit 1 ;;
 		v) echo -e "${script_name} v${script_version}" ; exit 1 ;;
 		\?) echo -e "\nInvalid option: -$OPTARG\n" >&2; echo -e $USAGE; exit 1 ;;
@@ -69,8 +87,12 @@ if [[ "${INTERVAL_COUNT}" -gt 1 ]];
 		GC_WINDOW=$l
 fi
 
-# calculate %GC for each interval of the input file
+#################################################################################
+# Calculate %GC for each interval of the input file if not previously done
+#################################################################################
+
 echo -ne "Calculate %GC for each interval of the input file..."
+
 if [[ -z "${INPUT_NAME}.withGCcontent.bed" || ! -f "${INPUT_NAME}.withGCcontent.bed" ]];
 then
 	bedtools nuc -fi ${GENOME} -bed "${INPUT_FILE}" \
@@ -82,42 +104,65 @@ else
 	echo -e "Already done"
 fi
 
+#################################################################################
 # Store all the possible %GC found in input file and their number of occurence
+#################################################################################
+
 echo -ne "Store all the possible %GC found in input file and their number of occurence..."
+
 list_percent=$( awk '$1!~/^#/ {print $7}' "${INPUT_NAME}.withGCcontent.bed" | sort -k1,1n | uniq | awk -F"\n" '{printf $1" "}' | sed 's/.$//' )
 declare -A nb
 for k in ${list_percent};
 do
 	nb[${k}]=$( awk -v k=$k '$NF==k' "${INPUT_NAME}.withGCcontent.bed" | wc -l )
 done
+
 echo -e "Done"
 
+
+#################################################################################
 # Generate random insertions matching GC content of input file
+#################################################################################
+
 echo -ne "Generate random insertions matching GC content of input file..."
+
 r=${NB_INSERTIONS}
 tmp=""
 while [ "$r" -gt 0 ] ;
 do
-	newline=$( bedtools random -l ${GC_WINDOW} -n 1 -g "${GENOME_DIR}/${GENOME_NAME}.genome" \
-	| bedtools nuc -fi ${GENOME} -bed - \
+	newline=$( \
+	bedtools shuffle -incl ${ALLOWED} -noOverlapping -i "${INPUT_FILE}" -g "${GENOME_DIR}/${GENOME_NAME}.genome" \
+	| head -1 \
+	| bedtools nuc -fi "${GENOME_DIR}/${GENOME_NAME}.fa" -bed - \
 	| tail -1 \
-	| awk '($1!~/^#/ && $13==0) {for (i=1;i<=6;i++) {printf $i "\t"}; printf "%.02f\n",$8}' )
+	| awk -v p=${GC_WINDOW} '($1!~/^#/ && $13==0) {printf $1 "\t" $2 "\t" $3 "\t" "1" "\t" p "\t" "%.02f\n",$8}' \
+	)
+
 	p=$( echo "$newline" | awk '{printf $NF}' )
+
+	# add interval if line is non empty and if the corresponding base composition is not fully represented yet in the output
 	if [[ -n "$p" ]]
 		then
 		if [[ "${nb[${p}]}" -gt 0 ]]
 		then
 			tmp="$tmp\n$newline"
+			# print progression
 			if (( $r % 100 == 0 ))
 			then
-				echo -ne "|"
+				echo -ne "."
 			fi
 			(( --r ))
 			(( --nb[${p}] ))
 		fi
 	fi
 done
+
+# reformat and sort output file before saving it
+echo -e "$tmp" \
+| awk '$1!=""' \
+| sort -k1,1 -k2,2n \
+> "${OUTPUT_FILE}"
+
 echo -e "...Done"
 
-echo -e "$tmp" | awk '$1!=""' | sort -k1,1 -k2,2n > "${OUTPUT_FILE}"
 exit
